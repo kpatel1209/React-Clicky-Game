8d0a4b22a7da21e46db91bdf1def77fd88df4083
Kevin Patel <kpatel1209@gmail.com>
2019-04-08 12:28:36 -0400

    adjusted width of cards

--- src/App.js
+++ src/App.js
@@ -62,6 +62,7 @@ class App extends Component {
             {this.shuffleLogo().map(team => (
               <TeamCard
                 confirmClick={this.confirmClick}
+                id={team.id}
                 name={team.name}
                 logo={team.logo}    
               />
